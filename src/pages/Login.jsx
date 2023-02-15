7f8b09113036b3dfc0aa1f7dcc668890cdf38058
pbit <alstjqrhkr2018@gmail.com>
2023-02-15 23:53:23 +0900

    kakao login UI

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Login() {
+
+  const handleLogin = () => {
+    
+  };
+
+  return (
+    <div>
+      
+      <button className='kakaoBtn' onClick={handleLogin}>
+        <img className='kakaoStartIcon' src={"/img/start_kakao.png"} alt="이미지가 없습니다" />
+      </button>
+      
+    </div>
+  );
+}
+
